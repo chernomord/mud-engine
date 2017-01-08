2dd1bdface34af4a93bdb747816656974b9f46cd
Slava <horlet@gmail.com>
2017-01-08 22:04:45 +0300

    #1 yaml file reader

--- play.py
+++ play.py
@@ -1,61 +1,9 @@
 from dungeon import Dungeon
+import yaml
 
-dungeon_data = {
-    'entry': 'Hall',
-    'exit': 'END',
-    'rooms': {
-        'Hall': {
-            'description': 'You enter the hall',
-            'doors': {
-                'Cellar': {
-                    'lead_to': 'Cellar',
-                    'description': 'Trapdoor to some kind of castle basement'
-                },
-                'Passage': {
-                    'lead_to': 'Passage',
-                    'description': 'Dark and gloomy pass into the Corridor'
-                }
-            }
-        },
-        'Cellar': {
-            'description': 'You walk down the stairs and the trapdoor shuts above your head. It is so dark here that you can\'t see anything.',
-            'doors': {
-                'Hall': {
-                    'lead_to': 'Hall',
-                    'description': 'Stairs up to the Hall'
-                }
-            }
-        },
-        'Passage': {
-            'description': 'You enter dark and narrow passage leading somewhere deeper into the Castle. There\'are boockshelf alonside the door and the armor stand in the corner',
-            'doors': {
-                'Hall': {
-                    'lead_to': 'Hall',
-                    'description': 'Doorway to the Hall'
-                },
-                'Inner garden': {
-                    'lead_to': 'Inner garden',
-                    'description': 'Dark doorway. Wind silently blows from it\' direction and you can hear some strange eery sounds going from that direction...'
-                }
-            }
-        },
-        'Inner garden': {
-            'description': 'It is the end. The night, the dark, the stars and full bright glowing moon and the sounds of night and it\'s wildlife.',
-            'doors': {
-                'Passage': {
-                    'lead_to': 'Passage',
-                    'description': 'Rush back to the safety of the Castle'
-                },
-                'END': {
-                    'lead_to': 'END',
-                    'description': 'Embrace the Moon and the howling of the beasts!'
-                }
-            }
-        },
-        'END': {
-            'description': 'You became one with the Night...'
-        }
-    }
-}
 
-Dungeon(dungeon_data).run()
+with open("dungeon.yaml", 'r') as stream:
+    try:
+        Dungeon(yaml.load(stream)).run()
+    except yaml.YAMLError as exc:
+        print(exc)
